28f96d09c5d9c2d9207301dbb9786c8d39b1d8b5
alex <aliaksandr.zakharau@centage.com>
2024-06-02 13:03:58 +0200

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://telegram-coin-mini-app.web.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 3000);
 }
